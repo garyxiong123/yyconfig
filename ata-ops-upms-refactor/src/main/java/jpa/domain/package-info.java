e3f6e0de77f051354c2cb795cefc624b9d7b88fe
熊成威 <xiongchengwei@yofish.com>
2019-11-22 11:18:31 +0800

    fix

--- ata-ops-upms-refactor/src/main/java/jpa/domain/package-info.java
+++
@@ -1,6 +0,0 @@
-
-/**
- * @Author: xiongchengwei
- * @Date: 2019/10/9 下午5:12
- */
-package jpa.domain;
--- ata-ops-upms-refactor/src/main/java/jpa/strategy/package-info.java
+++
@@ -1,5 +0,0 @@
-/**
- * @Author: xiongchengwei
- * @Date: 2019/10/17 上午8:43
- */
-package jpa.strategy;
