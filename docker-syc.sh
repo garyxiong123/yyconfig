c0b26493526f39b03550141c386abdef1d1a8d1f
熊成威 <xiongchengwei@yofish.com>
2019-12-04 17:33:32 +0800

    fix

---
+++ docker-syc.sh
@@ -0,0 +1,12 @@
+
+#!/usr/bin/env bash
+
+echo 'ss'
+
+ mvn clean install -Dmaven.test.skip=true
+
+docker build -t garyxiong/apollo-mini:test -f docker/Dockerfile .
+
+docker push garyxiong/apollo-mini:test
+
+docker run -it garyxiong/apollo-mini:test
