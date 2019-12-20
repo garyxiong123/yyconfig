06e6e51f00107d6f7d3273423f2cdd8daa50f87f
rache <f-f2001@163.com>
2019-12-20 16:38:22 +0800

    去除name

--- apollo/apollo-portal/apollo-portal-biz/src/main/java/com/yofish/apollo/dto/CreateItemReq.java
+++ apollo/apollo-portal/apollo-portal-biz/src/main/java/com/yofish/apollo/dto/CreateItemReq.java
@@ -16,14 +16,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 @Builder
 @Data
 public class CreateItemReq {
-    private String appId;
-    private String env;
-    private String clusterName;
-    private String namespaceName;
     private String key;
-
     private String value;
-
     private String comment;
     private Long appEnvClusterNamespaceId;
     private Integer lineNum;
