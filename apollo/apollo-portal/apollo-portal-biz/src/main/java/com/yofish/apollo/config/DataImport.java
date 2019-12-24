1654af7633f4317fcc04f38957fe761f58e44997
熊成威 <xiongchengwei@yofish.com>
2019-12-24 19:31:06 +0800

    新增config

--- apollo/apollo-portal/apollo-portal-biz/src/main/java/com/yofish/apollo/config/DataImport.java
+++ apollo/apollo-portal/apollo-portal-biz/src/main/java/com/yofish/apollo/config/DataImport.java
@@ -1,23 +1,32 @@
 package com.yofish.apollo.config;
 
+import com.google.common.collect.Sets;
 import com.yofish.apollo.domain.*;
+import com.yofish.apollo.dto.CreateItemReq;
 import com.yofish.apollo.enums.ServerConfigKey;
 import com.yofish.apollo.repository.*;
 import com.yofish.apollo.service.AppService;
+import com.yofish.apollo.service.ItemService;
 import com.yofish.apollo.service.NamespaceBranchService;
-import framework.apollo.core.enums.ConfigFileFormat;
+import com.yofish.apollo.service.ReleaseService;
+import com.yofish.gary.biz.domain.User;
+import com.yofish.gary.biz.repository.UserRepository;
+import common.dto.NamespaceDTO;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
 import org.springframework.util.ObjectUtils;
 
 import javax.annotation.PostConstruct;
+import java.util.List;
 
 /**
  * @author WangSongJun
  * @date 2019-12-17
  */
 @Slf4j
+@Order()
 @Component("apolloDataImport")
 public class DataImport {
     @Autowired
@@ -31,9 +40,17 @@ public class DataImport {
     @Autowired
     private AppEnvClusterNamespaceRepository namespaceRepository;
     @Autowired
+    private AppEnvClusterNamespace4BranchRepository namespace4BranchRepository;
+    @Autowired
     private AppEnvClusterRepository clusterRepository;
     @Autowired
     private AppService appService;
+    @Autowired
+    private UserRepository userRepository;
+    @Autowired
+    private ItemService itemService;
+    @Autowired
+    private ReleaseService releaseService;
 
     @Autowired
     private NamespaceBranchService namespaceBranchService;
@@ -44,6 +61,7 @@ public class DataImport {
     private final String defaultAppName = "中台支付";
     private final String defaultAppCode = "payment";
     private final String defaultNamespaceName = "application";
+    private AppEnvClusterNamespace namespace;
 
     @PostConstruct
     public void activeDefaultEnvs() {
@@ -72,47 +90,77 @@ public class DataImport {
 
 
         App app = createDefaultApp(department);
-//        AppNamespace appNamespace = appNamespaceRepository.findByAppAndName(app, defaultNamespaceName);
-//
-//
-//        AppEnvClusterNamespace namespace =  namespaceRepository.findByAppEnvClusterAndAppNamespace();
-//        namespaceBranchService.createBranch(namespace.getId(), "shanghai-DB");
+        AppEnvClusterNamespace4Branch namespace4Branch = createDefaultNamespace4Branch(app);
+
+
+        createFirstCreateItem(namespace);
+
+        createFirstRelease(namespace);
+
+        createSecondCreateItem(namespace);
+
+        createSecondRelease(namespace);
+
+        createThirdCreateItem(namespace);
+
 
     }
 
-    private AppEnvClusterNamespace createDefaultNamespace4Branch(AppEnvClusterNamespace namespace, AppEnvCluster appEnvCluster, AppNamespace appNamespace) {
-        AppEnvClusterNamespace4Branch namespace4Branch = new AppEnvClusterNamespace4Branch();
-        namespace4Branch.setParentId(namespace.getId());
-        namespace4Branch.setAppNamespace(appNamespace);
-        namespace4Branch.setAppEnvCluster(appEnvCluster);
-        namespaceRepository.save(namespace4Branch);
+    private void createThirdCreateItem(AppEnvClusterNamespace namespace) {
+        String releaseName = "releaseName3";
+        String releaseComment = "releaseComment3";
+        boolean isEmergencyPublish = false;
+        releaseService.publish(namespace,releaseName, releaseComment, null,  isEmergencyPublish);
+    }
+
+    private void createFirstCreateItem(AppEnvClusterNamespace namespace) {
+        CreateItemReq itemReq = CreateItemReq.builder().appEnvClusterNamespaceId(namespace.getId()).key("dbName").lineNum(1).value("payment").build();
+        itemService.createItem(itemReq);
+    }
+
+
+    private void createSecondRelease(AppEnvClusterNamespace namespace) {
+        String releaseName = "releaseName2";
+        String releaseComment = "releaseComment2";
+        boolean isEmergencyPublish = false;
+        releaseService.publish(namespace,releaseName, releaseComment, null,  isEmergencyPublish);
+    }
+
+    private void createSecondCreateItem(AppEnvClusterNamespace namespace) {
+        CreateItemReq itemReq = CreateItemReq.builder().appEnvClusterNamespaceId(namespace.getId()).key("password").lineNum(2).value("123456").build();
+        itemService.createItem(itemReq);
+
+    }
+
+    private void createFirstRelease(AppEnvClusterNamespace namespace) {
+        String releaseName = "releaseName1";
+        String releaseComment = "releaseComment1";
+        boolean isEmergencyPublish = false;
+        releaseService.publish(namespace,releaseName, releaseComment, null,  isEmergencyPublish);
+
+    }
+
+    private AppEnvClusterNamespace4Branch createDefaultNamespace4Branch(App app) {
+
+
+        namespace = namespaceRepository.findAll().get(0);
+
+        NamespaceDTO namespaceDTO = namespaceBranchService.createBranch(namespace.getId(), "shanghai-DB");
+        AppEnvClusterNamespace4Branch namespace4Branch = namespace4BranchRepository.findAll().get(0);
         return namespace4Branch;
-    }
-
-    private AppEnvClusterNamespace createDefaultNamespace4Main(AppEnvCluster appEnvCluster, AppNamespace appNamespace) {
-        AppEnvClusterNamespace4Main namespace = new AppEnvClusterNamespace4Main();
-        namespace.setAppNamespace(appNamespace);
-        namespace.setAppEnvCluster(appEnvCluster);
-
-        namespaceRepository.save(namespace);
-        return namespace;
     }
 
 
     private App createDefaultApp(Department department) {
 
         App app = App.builder().name(defaultAppName).appCode(defaultAppCode).department(department).build();
+        List<User> users = userRepository.findAll();
+        app.setAppOwner(users.get(0));
+
+        app.setAppAdmins(Sets.newHashSet(users));
         appService.createApp(app);
         return app;
     }
-
-
-
-//
-//    private AppEnvClusterNamespace createDefaultAppEnvClusterNamespace(App app) {
-//
-//        return null;
-//    }
 
 
 }
