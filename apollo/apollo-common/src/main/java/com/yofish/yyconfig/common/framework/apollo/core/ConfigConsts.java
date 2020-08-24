b7c9d2275fae18ec9728215b7dfa391f51ab12d4
熊成威 <xiongchengwei@yofish.com>
2020-08-24 20:41:47 +0800

    调整包结构，  重构依赖注入，删除单测试

--- apollo/apollo-common/src/main/java/com/yofish/yyconfig/common/framework/apollo/core/ConfigConsts.java
+++ apollo/apollo-common/src/main/java/com/yofish/yyconfig/common/framework/apollo/core/ConfigConsts.java
@@ -19,7 +19,7 @@ public interface ConfigConsts {
     String NAMESPACE_APPLICATION = "application";
     String CLUSTER_NAME_DEFAULT = "default";
     String CLUSTER_NAMESPACE_SEPARATOR = "+";
-    String APOLLO_CLUSTER_KEY = "apollo.appEnvCluster";
+    String APOLLO_CLUSTER_KEY = "apollo.cluster";
     String YYCONFIG_META_KEY = "yyconfig.meta";
     String CONFIG_FILE_CONTENT_KEY = "content";
     String NO_APPID_PLACEHOLDER = "ApolloNoAppIdPlaceHolder";
